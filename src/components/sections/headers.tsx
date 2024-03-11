da4f265396fb76ba428009774eb504d8a7071680
YnfanteY2799 <nobu027.99@gmail.com>
2024-03-11 07:12:15 -0400

    Update headers.tsx

--- src/components/sections/headers.tsx
+++ src/components/sections/headers.tsx
@@ -7,17 +7,17 @@ import Link from "next/link";
 import type { ISectionHeadProps } from "@/types";
 import type { ReactNode } from "react";
 
-export default function SectionHeader({ Head, SubHead, MoreDetails }: ISectionHeadProps): ReactNode {
+export default function SectionHeader({ head, subHead, moreDetails }: ISectionHeadProps): ReactNode {
   return (
     <motion.div variants={TextVariants}>
       <p className="sm:text-[18px] text-[14px] text-primary text-xl uppercase tracking-wider font-local">
-        {SubHead}
+        {subHead}
       </p>
       <div className="flex flex-row">
         <h2 className="font-black md:text-[60px] sm:text-[50px] text-[30px] text-5xl z-0 flex font-local">
-          {Head}
-          {MoreDetails !== undefined && (
-            <Link href={MoreDetails} className="flex pl-[15px] pt-2">
+          {head}
+          {moreDetails !== undefined && (
+            <Link href={moreDetails} className="flex pl-4 pt-2">
               <CaretRight width={60} height={40} />
             </Link>
           )}
