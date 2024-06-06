2c22df0ad13e8138f9e9ab30687056dd80013f64
YnfanteY2799 <nobu027.99@gmail.com>
2024-06-06 08:19:04 -0400

    +adds

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -3,11 +3,14 @@ import { SectionHeader, SectionText } from "./parts";
 import { textRevealVariant } from "@/utils";
 
 import type { ReactNode } from "react";
+import { useTranslations } from "next-intl";
 
 export default function AboutSection(): ReactNode {
+  const t = useTranslations("About");
+
   return (
     <SectionWrapper id="About">
-      <SectionHeader head="About Me" subHead="Intro" moreDetails="About" />
+      <SectionHeader head={t("mainTitle")} subHead={t("subTitle")} moreDetails="About" />
       <SectionText className="mt-4 text-primary text-[17px] max-w-3xl leading-[30px]" variants={textRevealVariant}>
         About
       </SectionText>
