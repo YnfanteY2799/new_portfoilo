82846f97174ba030a7f06566745082de90a98e16
YnfanteY2799 <nobu027.99@gmail.com>
2024-06-09 12:32:42 -0400

    Update index.tsx

--- src/components/ui/Cards/TiltReveal/index.tsx
+++ src/components/ui/Cards/TiltReveal/index.tsx
@@ -1,6 +1,15 @@
 "use client";
+import { BaseTiltWrapper } from "@/components";
+import { cn, spawnFromRight } from "@/utils";
+import { motion } from "framer-motion";
 import { type ReactNode } from "react";
 
 export default function TiltRevealCard(): ReactNode {
-  return <div>TiltRevealCard</div>;
+  return (
+    <BaseTiltWrapper>
+      <motion.div variants={spawnFromRight} className={cn("w-20 bg-red-600 h-20")}>
+        TiltRevealCard
+      </motion.div>
+    </BaseTiltWrapper>
+  );
 }
